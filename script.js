43ea7181942c71d2456f8a5372ed5721a2a7bd97
magitech19 <143949452+magitech19@users.noreply.github.com>
2023-09-06 12:04:18 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,67 @@
+$(window).on("load", function() {
+  $(".pre-loader").addClass("none");
+})
+var words = ['Welcome to MagiTech', 'Here we provide multitude of services like;', 'Front-End Web Development', 'Back-end Web Development', 'Advice related to web services and much more...'],
+  part,
+  i = 0,
+  offset = 0,
+  len = words.length,
+  forwards = true,
+  skip_count = 0,
+  skip_delay = 15,
+  speed = 70;
+var wordflick = function() {
+  setInterval(function() {
+    if (forwards) {
+      if (offset >= words[i].length) {
+        ++skip_count;
+        if (skip_count == skip_delay) {
+          forwards = false;
+          skip_count = 0;
+        }
+      }
+    }
+    else {
+      if (offset == 0) {
+        forwards = true;
+        i++;
+        offset = 0;
+        if (i >= len) {
+          i = 0;
+        }
+      }
+    }
+    part = words[i].substr(0, offset);
+    if (skip_count == 0) {
+      if (forwards) {
+        offset++;
+      }
+      else {
+        offset--;
+      }
+    }
+    $('.word').text(part);
+  }, speed);
+};
+
+$(document).ready(function() {
+  wordflick();
+});
+
+$(function() {
+  
+  // contact form animations
+  $('#contact').click(function() {
+    $('#contactForm').slideToggle();
+  })
+  $(document).mouseup(function (e) {
+    var container = $("#contactForm");
+
+    if (!container.is(e.target) // if the target of the click isn't the container...
+        && container.has(e.target).length === 0) // ... nor a descendant of the container
+    {
+        container.fadeOut();
+    }
+  });
+  
+});
